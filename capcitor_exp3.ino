2e90f740d8abcb1d1c5da21bc51f61f4a710a2e9
Stephen Edem Adzoyi <stephenadzoyi31@gmail.com>
2025-09-07 21:41:36 +0000

    Add files via upload

---
+++ capcitor_exp3.ino
@@ -0,0 +1,10 @@
+const int capPin = 32; // Analog pin 
+void setup() { 
+Serial.begin(115200); 
+} 
+void loop() { 
+int capValue = analogRead(capPin); 
+Serial.print("Capacitor Voltage Reading: "); 
+Serial.println(capValue); 
+delay(200); 
+} 
